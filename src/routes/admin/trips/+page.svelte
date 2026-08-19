10c7c52af2930324bb4a5eb9a0946f94eb4bfad8
SplitACharter Scaffolder <scaffolder@splitacharter.com>
2026-08-19 11:15:20 -0400

    fix: update Refresh Data button icon to true circular double arrow on admin trips page

--- src/routes/admin/trips/+page.svelte
+++ src/routes/admin/trips/+page.svelte
@@ -334,10 +334,10 @@
 			title="Fetch latest trip and booking data from server"
 		>
 			<svg class:spinning={isRefreshing} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-				<path d="M21 12a9 9 0 0 0-9-9 9.75 9.75 0 0 0-6.74 2.74L3 8"/>
-				<path d="M3 3v5h5"/>
-				<path d="M3 12a9 9 0 0 0 9 9 9.75 9.75 0 0 0 6.74-2.74L21 16"/>
-				<path d="M16 21h5v-5"/>
+				<path d="M3 12a9 9 0 0 1 9-9 9.75 9.75 0 0 1 6.74 2.74L21 8"/>
+				<path d="M21 3v5h-5"/>
+				<path d="M21 12a9 9 0 0 1-9 9 9.75 9.75 0 0 1-6.74-2.74L3 16"/>
+				<path d="M8 21H3v-5"/>
 			</svg>
 			<span>{isRefreshing ? 'Refreshing...' : 'Refresh Data'}</span>
 		</button>
